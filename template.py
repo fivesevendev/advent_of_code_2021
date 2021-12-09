85320888ca4337db695e96ef659e332893caf22f
fivesevendev <github@fiveseven.dev>
2021-12-08 20:18:13 -0500

    Templates

---
+++ template.py
@@ -0,0 +1,25 @@
+#
+
+
+
+import sys, time
+
+
+#def numFind(n):
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    runStart = time.time()
+    print(time.asctime())
+    n = 0
+    print("Result:",numFind(n))
+    print("Run Time Was {:.2F} Seconds".format(time.time() - runStart))

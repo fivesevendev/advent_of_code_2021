e77f427f6c3827855003b4c7ce631e8bb57541fa
fivesevendev <github@fiveseven.dev>
2021-12-16 11:34:36 -0500

    Updated layout. Added output.txt to better accomodate large datasets

--- input_formatter.py
+++ input_formatter.py
@@ -6,7 +6,7 @@ def listOfLists(n):
         contents = unFormatted.read()
     formatted = contents.replace(" ", '''", "''')
     formatted = formatted.replace("\n", '''"],\n["''')
-    formatted = formatted.replace(''' "->",''', "")
+    #formatted = formatted.replace(''' "->",''', "")
     formatted = '''data = [\n["{}"]\n]'''.format(formatted)
     return formatted
 
@@ -14,7 +14,20 @@ def singleList(n):
     with open(n) as unFormatted:
         contents = unFormatted.read()
     formatted = contents.replace(" ", ", ")
-    formatted = "data = [{}]".format(formatted)
+    formatted = formatted.replace("\n", '''",\n"''')
+    formatted = '''data = ["{}"]'''.format(formatted)
+    return formatted
+
+def intListOfLists(n, o):
+    with open(n) as unFormatted:
+        contents = unFormatted.read()
+    formatted = str(list(contents))
+    formatted = formatted.replace(""", '\\n', """, """],\n[""")
+    formatted = formatted.replace("""'""", "")
+    formatted = formatted.replace(" ", "")
+    formatted = '''data = [\n{}\n]'''.format(formatted)
+    with open(o, mode='w') as output:
+        output.write(formatted)
     return formatted
 
 
@@ -23,6 +36,10 @@ if __name__ == '__main__':
     print(time.asctime())
     print()
     n = "input.txt"
-    print(listOfLists(n))
+    o = "output.txt"
+    #print(listOfLists(n))
+    #print(singleList(n))
+    print(intListOfLists(n, o))
+    
     print()
     print("Run Time Was {:.2F} Seconds".format(time.time() - runStart))

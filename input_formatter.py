132272d5d3d89d23fda794ab003f9b1c6e636f0c
fivesevendev <github@fiveseven.dev>
2021-12-24 14:47:40 -0500

    Input formatter

--- input_formatter.py
+++ input_formatter.py
@@ -2,21 +2,43 @@ import timeit, sys, time
 
 
 def strListOfLists(n, o):
-    pass
-
-def singleList(n, o):
-    pass
-
-def intListOfLists(n, o):
     with open(n) as unFormatted:
         formatted = unFormatted.read()
-    formatted = formatted.replace(" -> ", ",")
+    formatted = formatted.replace("""\n""", """],\n['""") ## New line for each row
+    #formatted = formatted.replace("""\n""", """],[""")  ## All rows on same line
+    formatted = formatted.replace(" ", """','""")
+    formatted = formatted.replace("..", ", ")
+    formatted = formatted.replace("x=", " ")
+    formatted = formatted.replace("y=", " ")
+    formatted = formatted.replace("z=", " ")
+    formatted = formatted.replace(""",'""", ",")
+    formatted = '''data = [\n['{}]\n]'''.format(formatted)
+    with open(o, mode='w') as output:
+        output.write(formatted)
+    return formatted
+
+def intSingleList(n, o):
+    with open(n) as unFormatted:
+        formatted = unFormatted.read()
     #formatted = str(list(formatted))
     #formatted = formatted.replace("""\n""", """],\n[""") ## New line for each row
     formatted = formatted.replace("""\n""", """],[""")  ## All rows on same line
     formatted = formatted.replace("""'""", "")
     formatted = formatted.replace(" ", "")
     formatted = '''data = [\n[{}]\n]'''.format(formatted)
+    with open(o, mode='w') as output:
+        output.write(formatted)
+    return formatted
+
+def intListOfLists(n, o):
+    with open(n) as unFormatted:
+        formatted = unFormatted.read()
+    formatted = str(list(formatted))
+    #formatted = formatted.replace(""" '\\n',""", """],\n[""") ## New line for each row
+    formatted = formatted.replace(""" '\\n',""", """],[""")  ## All rows on same line
+    formatted = formatted.replace("""'""", "")
+    formatted = formatted.replace(" ", "")
+    formatted = '''data = [\n{}\n]'''.format(formatted)
     with open(o, mode='w') as output:
         output.write(formatted)
     return formatted
@@ -28,8 +50,8 @@ if __name__ == '__main__':
     print()
     n = "input.txt"
     o = "output.txt"
-    #print(strListOfLists(n, o))
-    #print(singleList(n))
-    print(intListOfLists(n, o))
+    print(strListOfLists(n, o))
+    #print(intSingleList(n, o))
+    #print(intListOfLists(n, o))
     print()
     print("Run Time Was {:.4F} Seconds".format(timeit.default_timer() - startTime))

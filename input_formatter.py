af752611b85de4e8af18f838785c44e8eb63ef3c
fivesevendev <github@fiveseven.dev>
2021-12-24 21:44:07 -0500

    Input formatter

--- input_formatter.py
+++ input_formatter.py
@@ -1,18 +1,25 @@
 import timeit, sys, time
 
 
+def strSingleList(n, o):
+    with open(n) as unFormatted:
+        formatted = unFormatted.read()
+    formatted = str(list(formatted))
+    formatted = '''data = {}'''.format(formatted)
+    with open(o, mode='w') as output:
+        output.write(formatted)
+    return formatted
+
 def strListOfLists(n, o):
     with open(n) as unFormatted:
         formatted = unFormatted.read()
-    formatted = formatted.replace("""\n""", """],\n['""") ## New line for each row
+    formatted = str(list(formatted))
+    formatted = formatted.replace(""", '\\n'""", """],\n['""") ## New line for each row
     #formatted = formatted.replace("""\n""", """],[""")  ## All rows on same line
-    formatted = formatted.replace(" ", """','""")
-    formatted = formatted.replace("..", ", ")
-    formatted = formatted.replace("x=", " ")
-    formatted = formatted.replace("y=", " ")
-    formatted = formatted.replace("z=", " ")
-    formatted = formatted.replace(""",'""", ",")
-    formatted = '''data = [\n['{}]\n]'''.format(formatted)
+    #formatted = formatted.replace(" ", """','""")
+    #formatted = formatted.replace("..", ", ")
+    #formatted = formatted.replace(""",'""", ",")
+    formatted = '''data = [\n{}\n]'''.format(formatted)
     with open(o, mode='w') as output:
         output.write(formatted)
     return formatted
@@ -50,6 +57,7 @@ if __name__ == '__main__':
     print()
     n = "input.txt"
     o = "output.txt"
+    #print(strSingleList(n, o))
     print(strListOfLists(n, o))
     #print(intSingleList(n, o))
     #print(intListOfLists(n, o))
